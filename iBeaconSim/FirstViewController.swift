49631a61b81c2444c5e98ef133118b50c4304469
Yumen Cao <gawainus97@gmail.com>
2015-01-25 16:36:12 -0600

    Initial Commit

---
+++ iBeaconSim/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iBeaconSim
+//
+//  Created by Gawain Tsao on 1/25/15.
+//  Copyright (c) 2015 Gawain Tsao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
